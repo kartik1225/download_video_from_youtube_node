e7ca6354156906a7e0cfdf68c9d25c75ec2a8b8b
kartik garasia <mac@Macs-MacBook-Pro.local>
2017-11-10 19:20:27 +0530

    Initial commit

---
+++ src/components/bodyTemplate.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      light
+      overflow
+    >
+
+      <v-list dense class="pt-0">
+              <v-list-tile v-for="item in items" :key="item.title" href :to='item.href' @click="">
+                <v-list-tile-action>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+      </v-list>
+
+    </v-navigation-drawer>
+    <v-toolbar fixed class="elevation-1 blue-grey darken-3"  app clipped-left dark>
+      <v-toolbar-side-icon class='white--text' @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title role='button' class='white--text' @click='$router.push("/")'><strong>w3</strong>saver</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-slide-x-transition>
+
+       <router-view style='margin-top:64px;'></router-view>
+      </v-slide-x-transition>
+    </main>
+
+    <v-footer class="pa-3" >
+
+      <v-spacer></v-spacer>  
+      <div> <strong>w3</strong>saver.com <strong>|</strong> © {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'w3saver', icon: 'home', href:'/' },
+          { title: 'Contact Us', icon: 'email', href:'/contact' },
+          { title:'bug report', icon: 'bug_report', href:'/bug' }
+        ],
+        right: null
+      }
+    }
+  }
+</script>
